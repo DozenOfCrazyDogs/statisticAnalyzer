4a4f7de35f4ff4a2c83c83d67624f7aff334e7ee
chronoss <igorkravchenko93@gmail.com>
2016-07-09 19:50:28 +0300

    fix pomxml

--- src/main/java/statisticanalyzer/domain/Message.java
+++ src/main/java/statisticanalyzer/domain/Message.java
@@ -1,22 +1,19 @@
 package statisticanalyzer.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Igor on 24.06.2016.
  */
 public class Message {
+    @Getter
+    @Setter
     String name;
     String text;
 
     public Message(String name, String text) {
         this.name = name;
         this.text = text;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getText() {
-        return text;
     }
 }
--- src/test/java/statisticanalyzer/service/impl/MatrixTranponationsIgorTest.java
+++ src/test/java/statisticanalyzer/service/impl/MatrixTranponationsIgorTest.java
@@ -1,6 +1,5 @@
 package statisticanalyzer.service.impl;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -13,14 +12,13 @@ public class MatrixTranponationsIgorTest {
     @Test
     public void myMeth() {
         int[] matrix = new int[]
-                {11, 12, 13, 14
-                };
+                {11, 12, 13, 14};
 
         MatrixTranponationsIgor obj = new MatrixTranponationsIgor();
 
         int[] result = obj.transponate(matrix);
 
-        int[] expected = {14,12,13,11};
+        int[] expected = {14, 12, 13, 11};
 
         assertArrayEquals(expected, result);
     }
